5d27335ac97afee17bdb7f15084a83a9fa06a151
Will Norris <will@biohack.net>
2026-04-12 06:14:36 +0700

    not needed with update

--- wftools/GNUfixlex.pl
+++
@@ -1,4 +0,0 @@
-s/malloc\( size \);/new char[size];/;
-s/return \(void \*\) realloc\( \(char \*\) ptr, size \);/assert\(0\); return 0;/;
-s/free\( ptr \);/assert\(ptr\); delete [] ptr;/;
-s/class istream;/#include <iostream>\nusing namespace std;/;
